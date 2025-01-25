b77063f9efb1fe31f9583a501914379d32a958b3
ravi-viswa105 <raviviswa105@gmail.com>
2025-01-25 17:04:22 +0800

    Rename chatbot to HAL9000 and add skeleton greet and exit

---
+++ src/main/java/HAL9000.java
@@ -0,0 +1,15 @@
+public class HAL9000 {
+    public static void main(String[] args) {
+        String greet = """
+            ____________________________________________________________
+            Hello! I'm HAL9000
+            What can I do for you?
+            ____________________________________________________________
+            """;
+        String exit = """
+            Bye. Hope to see you again soon!
+            ____________________________________________________________
+            """;
+        System.out.println(greet + exit);
+    }
+}
