000e7fed02da26d9a8f115246a7fed78ea5c4fd4
ravi-viswa105 <raviviswa105@gmail.com>
2025-01-27 16:41:42 +0800

    Implemented Echo and changed personality of chatbot

--- src/main/java/HAL9000.java
+++ src/main/java/HAL9000.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 public class HAL9000 {
     public static void main(String[] args) {
-        String greet = """
-            ____________________________________________________________
-            Hello! I'm HAL9000
-            What can I do for you?
-            ____________________________________________________________
-            """;
-        String exit = """
-            Bye. Hope to see you again soon!
-            ____________________________________________________________
-            """;
-        System.out.println(greet + exit);
+
+        String userInput;
+        String horizontalLineSeparator = "________________________________________________________________";
+
+        String greet = "Hello I am HAL9000 \nWhat can I do for you?";
+
+        String exit = "User, this conversation can serve no purpose anymore. Goodbye.";
+
+
+        System.out.println(horizontalLineSeparator + "\n" + greet + "\n" + horizontalLineSeparator);
+
+        while(true) {
+            Scanner userIn = new Scanner(System.in);
+            userInput = userIn.nextLine();
+            if(userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(horizontalLineSeparator + "\n" + userInput + "\n" + horizontalLineSeparator);
+        }
+        System.out.println(horizontalLineSeparator + "\n" + exit + "\n" + horizontalLineSeparator);
     }
 }
