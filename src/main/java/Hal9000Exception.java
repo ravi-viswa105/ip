eb74eae7b8480cb66b88d0c920bc3bb9936f128f
ravi-viswa105 <raviviswa105@gmail.com>
2025-02-11 22:43:26 +0800

    Add error handling

---
+++ src/main/java/Hal9000Exception.java
@@ -0,0 +1,11 @@
+public class Hal9000Exception extends Exception {
+    public Hal9000Exception(String message) {
+        super(message);
+    }
+
+    public void printException() {
+        String lineSeparator = "________________________________________________________________";
+        System.out.println(lineSeparator + "\n" + getMessage() + "\n" + lineSeparator);
+    }
+
+}
--- src/main/java/UserInputParser.java
+++ src/main/java/UserInputParser.java
@@ -8,7 +8,7 @@ public class UserInputParser {
         inputSplit = userInput.split(" ");
     }
 
-    public TaskType findTaskType() {
+    public TaskType findTaskType() throws Hal9000Exception {
         return switch (inputSplit[0]) {
             case "todo" -> TaskType.TODO;
             case "deadline" -> TaskType.DEADLINE;
@@ -17,35 +17,57 @@ public class UserInputParser {
             case "unmark" -> TaskType.UNMARK;
             case "bye" -> TaskType.BYE;
             case "list" -> TaskType.LIST;
-            default -> TaskType.NONE;
+            default -> throw new Hal9000Exception("I'm sorry User. I'm afraid I can't do that");
         };
     }
 
-    public String findTodoTaskName() {
+    public String findTodoTaskName() throws Hal9000Exception {
+        String filteredTaskName = userInput.substring(4).trim();
+        if (filteredTaskName.isEmpty()) {
+            throw new Hal9000Exception("User, the task name cannot be empty");
+        }
         return userInput.substring(4).trim();
     }
 
-    public String findDeadlineTaskName() {
-        String filteredTaskName = userInput.split("/by")[0];
-        return filteredTaskName.substring(8).trim();
+    public String findDeadlineTaskName() throws Hal9000Exception {
+        String filteredTaskName = userInput.split("/by")[0].substring(8).trim();
+        if (filteredTaskName.isEmpty()) {
+            throw new Hal9000Exception("User, the task name cannot be empty");
+        }
+        return filteredTaskName;
     }
 
-    public String findEventTaskName() {
-        String filteredTaskName = userInput.split("/from")[0];
-        return filteredTaskName.substring(5).trim();
+    public String findEventTaskName() throws Hal9000Exception {
+        String filteredTaskName = userInput.split("/from")[0].substring(5).trim();
+        if (filteredTaskName.isEmpty()) {
+            throw new Hal9000Exception("User, the task name cannot be empty");
+        }
+        return filteredTaskName;
     }
 
-    public String findDeadlineBy() {
-        return userInput.split("/by")[1].trim();
+    public String findDeadlineBy() throws Hal9000Exception {
+        String[] splitUserInput = userInput.split("/by");
+        if (splitUserInput.length < 2) {
+            throw new Hal9000Exception("Sorry User, you forgot to include a /by time");
+        }
+        return splitUserInput[1].trim();
     }
 
-    public String findEventFrom() {
+    public String findEventFrom() throws Hal9000Exception {
         String filteredTaskName = userInput.split("/to")[0].trim();
-        return filteredTaskName.split("/from")[1].trim();
+        String[] splitUserInput = filteredTaskName.split("/from");
+        if (splitUserInput.length < 2) {
+            throw new Hal9000Exception("Sorry User, you forgot to include a /from time");
+        }
+        return splitUserInput[1].trim();
     }
 
-    public String findEventTo() {
-        return userInput.split("/to")[1].trim();
+    public String findEventTo() throws Hal9000Exception {
+        String[] splitUserInput = userInput.split("/to");
+        if (splitUserInput.length < 2) {
+            throw new Hal9000Exception("Sorry User, you forgot to include a /to time");
+        }
+        return splitUserInput[1].trim();
     }
 
     public int findMarkIndex() {
